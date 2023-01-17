cff833a6db74cfa33c07effb5d954188494c6246
Luca SOLER <92515171+lucasolerr@users.noreply.github.com>
2023-01-17 16:51:14 +0100

    Update parsing-switch-info.py

--- parsing-switch-info.py
+++ parsing-switch-info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import paramiko
 import os
 
 df = pd.DataFrame(columns=["Hostname", "Vendor", "Product Name", "Serial Number", "MAC Address", "Software Revision", "Up Time", "CPU Util"])
@@ -120,3 +121,31 @@ for filename in os.listdir(directory):
 
 # Export du DataFrame vers un fichier Excel
 df.to_excel('output.xlsx', index=False)
+
+
+# Création d'une nouvelle instance SSHClient
+client = paramiko.SSHClient()
+
+# Ajout de la clé SSH de la machine distante au "known_hosts"
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Connexion à la machine distante
+client.connect(hostname='10.50.33.21', username='user-ansible', password='password')
+
+# Ouverture d'un SFTP session
+sftp = client.open_sftp()
+
+# Chemin du fichier local à exporter
+local_file = './output.xlsx'
+
+# Chemin de destination sur la machine distante
+remote_file = './'
+
+# Transfert du fichier
+sftp.put(local_file, remote_file)
+
+# Fermeture de la session SFTP
+sftp.close()
+
+# Fermeture de la connexion SSH
+client.close()
