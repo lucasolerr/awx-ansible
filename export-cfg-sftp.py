77a5d31938f16f82fa1d0705796386bac000d65a
Luca SOLER <92515171+lucasolerr@users.noreply.github.com>
2023-01-18 10:46:01 +0100

    Create export-cfg-sftp.py

---
+++ export-cfg-sftp.py
@@ -0,0 +1,26 @@
+# Création d'une nouvelle instance SSHClient
+client = paramiko.SSHClient()
+
+# Ajout de la clé SSH de la machine distante au "known_hosts"
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Connexion à la machine distante
+client.connect(hostname='10.50.33.21', username='user-ansible', password='password')
+
+# Ouverture d'un SFTP session
+sftp = client.open_sftp()
+
+# Chemin du fichier local à exporter
+local_file = './*.cfg'
+
+# Chemin de destination sur la machine distante
+remote_file = './cfg'
+
+# Transfert du fichier
+sftp.put(local_file, remote_file)
+
+# Fermeture de la session SFTP
+sftp.close()
+
+# Fermeture de la connexion SSH
+client.close()
